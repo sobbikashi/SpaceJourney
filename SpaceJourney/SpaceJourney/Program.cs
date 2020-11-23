3ca2e4be49b5184ca1555bae7a51901273950ae9
SobbikashiGit <xt207@yandex.ru>
2020-11-23 14:23:37 +0300

    Добавлен сплеш-скрин

--- SpaceJourney/SpaceJourney/Program.cs
+++ SpaceJourney/SpaceJourney/Program.cs
@@ -11,13 +11,25 @@ namespace SpaceJourney
         [STAThread]
         static void Main()
         {
-            Form form = new Form();
-            form.Width = 1280;
-            form.Height = 768;
-            Game.Init(form);
-            form.Show();
-            Game.Draw();
-            Application.Run(form);
+            //Form form = new Form();
+            //form.Width = 1280;
+            //form.Height = 768;
+            //Game.Init(form);
+            //form.Show();
+            //Game.Draw();
+            //Application.Run(form);
+            using (Form SplashScreen = new SplashScreen())
+            {
+                try
+                {
+                    Application.Run(SplashScreen);
+                }
+                catch (Exception ex)
+                {
+                    Application.Exit();
+                }
+            }
+
 
         }
     }
---
+++ SpaceJourney/SpaceJourney/SplashScreen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceJourney
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form MainWindow = new Form();
+            MainWindow.Width = 1280;
+            MainWindow.Height = 768;
+            Game.Init(MainWindow);
+            MainWindow.Show();
+            Game.Draw();
+            this.Visible = false;
+        }
+    }
+}

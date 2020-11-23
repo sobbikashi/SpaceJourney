7fb63d979c9201d00f8c820f2a02c841b522e94a
SobbikashiGit <xt207@yandex.ru>
2020-11-23 14:03:56 +0300

    Создан проект

---
+++ SpaceJourney/SpaceJourney/Game.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace SpaceJourney
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+        // Свойства
+        // Ширина и высота игрового поля
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        static Game()
+        {
+        }
+        public static void Init(Form form)
+        {
+            // Графическое устройство для вывода графики            
+            Graphics g;
+            // Предоставляет доступ к главному буферу графического контекста для текущего приложения
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            // Создаем объект (поверхность рисования) и связываем его с формой
+            // Запоминаем размеры формы
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            // Связываем буфер в памяти с графическим объектом, чтобы рисовать в буфере
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+        }
+        public static void Draw()
+        {
+            // Проверяем вывод графики
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            Buffer.Render();
+        }
+
+    }
+}
---
+++ SpaceJourney/SpaceJourney/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SpaceJourney
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form form = new Form();
+            form.Width = 1280;
+            form.Height = 768;
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+
+        }
+    }
+}
